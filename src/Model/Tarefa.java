50097766708fbb8dbb56e9d064c94b5171484c82
tiagoluzh607 <tiagoluz.h607@gmail.com>
2018-06-22 16:54:46 -0300

    Criando a Classe Tarefa Com um metodo executar, adicionando em uma listae chamando o executar

---
+++ src/Model/Tarefa.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author tiago
+ */
+public class Tarefa {
+    
+    private String nome;
+    private int tempoExecucao;
+    private int quantum;
+
+    public Tarefa(String nome, int tempoExecucao, int quantum) {
+        this.nome = nome;
+        this.tempoExecucao = tempoExecucao;
+        this.quantum = quantum;
+    }
+    
+    public void Executar(){
+        
+        for (int i = 0; i < this.quantum; i++) {
+            tempoExecucao--;
+            System.out.println("Nome: "+ this.nome + " | TempoRestante: " +tempoExecucao);
+            Dormir();
+        }
+        
+    }
+
+    private void Dormir() {
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Tarefa.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+}
---
+++ src/roundrobin/RoundRobin.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package roundrobin;
+
+import Model.Tarefa;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author tiago
+ */
+public class RoundRobin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ArrayList<Tarefa> tarefas = new ArrayList();
+        
+        
+        Tarefa escovarOsDentes = new Tarefa("escovarOsDentes",10, 2);
+        tarefas.add(escovarOsDentes);
+        
+        
+        
+        for (Tarefa tarefa : tarefas) {
+            tarefa.Executar();
+        }
+        
+        
+            
+    }
+    
+}
